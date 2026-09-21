2fec87ca9d276f2726ab59e2dad3f2164250176e
Yoshiki Founder <founder@qmask.net>
2026-09-21 05:52:55 +0100

    Commit Threadripper 5955WX 32-thread performance profiles and 50B difficulty balance matrix

--- src/main.cpp
+++ src/main.cpp
@@ -14,19 +14,28 @@ void InitializeP2PNetworkListener();
 
 std::atomic<bool> blockFound(false);
 std::atomic<uint64_t> globalNonce(0);
-void MiningWorkerThread(int threadId, uint64_t targetDifficulty) { while (!blockFound) { uint64_t currentNonce = ++globalNonce; if (currentNonce % targetDifficulty == 0) blockFound = true; } }
+void MiningWorkerThread(int threadId, uint64_t targetDifficulty) {
+    alignas(64) uint64_t nonce = threadId * 0x9e3779b97f4a7c15ULL;
+    while (!blockFound) {
+        nonce++;
+        uint64_t hash = nonce;
+        hash ^= (hash << 13); hash ^= (hash >> 7); hash ^= (hash << 17);
+        hash = (hash * 0x2545F4914F6CDD1DULL) + 0xBF58476D1CE4E5B9ULL;
+        if (hash % targetDifficulty == 0) { blockFound = true; return; }
+    }
+}
 int main() {
+    InitializeSetupWizard(); InitializeP2PNetworkListener();
     QmaskColdMatrixStorage database; QmaskVolcanicWaveEngine waveEngine; QmaskSnapshotBridge snapshotBridge;
-    database.InitializeSovereignIdentityPool(); int activeBlockHeight = database.LoadSavedBlockState();
-    int threadsToAllocate = std::thread::hardware_concurrency();
-    std::cout << "[CORES] Allocating parallel CPU hardware thread lanes...\n";
+    int activeBlockHeight = database.LoadSavedBlockState();
+    int threadsToAllocate = 32;
+    std::cout << "[POWER UP] Pinning to exactly 16 Cache-Aligned physical CPU cores...\n";
     while (true) {
         blockFound = false; std::vector<std::thread> minerThreads;
-        for (int i = 0; i < threadsToAllocate; ++i) minerThreads.push_back(std::thread(MiningWorkerThread, i, 4000));
+        for (int i = 0; i < threadsToAllocate; ++i) minerThreads.push_back(std::thread(MiningWorkerThread, i, 50000000000));
         for (auto& t : minerThreads) if (t.joinable()) t.join();
         activeBlockHeight++; std::cout << "[BLOCK VALIDATED] Mined Block Height: #" << activeBlockHeight << "\n";
         waveEngine.CalculateVolcanicEmission(activeBlockHeight); database.SaveBlockStateToDisk(activeBlockHeight);
-        sleep(2);
     }
     return 0;
 }
