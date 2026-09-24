f08630180e48bc838d246a2e217b95d614c861dc
yoshigold <yoshikiazuma@protonmail.com>
2026-09-24 11:18:41 +0100

    Delete official moneu modded with qmask beta build backup directory

--- official moneu modded with qmask beta build backup/src/decay_manager.h
+++
@@ -1,7 +0,0 @@
-#include <string>
-
-class QmaskDecayManager {
-public:
-    // 🛡️ Manages updating the ledger state and calculating deflationary yields for ghost wallets
-    void AuditLedgerInactivity(const std::string& accountName, double activeBalance, int idleBlocks);
-};
